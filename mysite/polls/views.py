f8f525ab293c9e4f2500ec8069ad9690fbf32c20
Unikorn996 <gemme_38@hotmail.com>
2022-09-13 19:13:04 +0700

    docstrings added to views.py and models.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,20 +10,26 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    """View of the index page"""
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
+        """Return all questions sorted by published date."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
+    """View of the detail page"""
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
+        """Return all questions sorted by published date."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
     def page_redirection(self, request, **kwargs):
+        """Redirect the not-allowed page to the index page."""
         try:
             question = Question.objects.get(pk=kwargs['pk'])
             if not question.can_vote():
@@ -33,11 +39,13 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
+    """View of the result page"""
     model = Question
     template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
+    """Vote the selected poll."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
