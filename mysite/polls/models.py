c17e432b31d216289b2916188dac180e81178ce1
Unikorn996 <gemme_38@hotmail.com>
2022-09-13 13:48:03 +0700

    two methods and one attribute added to model.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,10 +6,21 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('end date', null=True, default=timezone.localtime())
 
     def was_published_recently(self):
-        now = timezone.now()
+        now = timezone.localtime()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_publsihed(self):
+        now = timezone.localtime()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        now = timezone.localtime()
+        if self.end_date == None:
+            return self.is_published()
+        return self.pub_date >= now >= self.end_date
 
     def __str__(self):
         return self.question_text
