3fd2a784c7d0848b934a8b05f586946296abbbd6
Unikorn996 <gemme_38@hotmail.com>
2023-09-10 17:22:28 +0700

    add end_date and can_vote in Question class

--- polls/models.py
+++ polls/models.py
@@ -5,11 +5,22 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    end_date = models.DateTimeField('date ended', blank=True)
     
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def is_published(self):
+        now = timezone.now()
+        return self.pub_date <= now
+    
+    def can_vote(self):
+        now = timezone.now()
+        if self.end_date is None:
+            return self.is_published()
+        return self.pub_date <= now <= self.end_date
 
     def __str__(self):
         return self.question_text
